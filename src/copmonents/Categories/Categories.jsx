a45e49987ce05b8b5de2cb0e73dea1ac3909d42e
wintermute28 <a28c@yandex.ru>
2024-06-09 14:39:14 +0300

    fix slider and banners scroll

--- src/copmonents/Categories/Categories.jsx
+++ src/copmonents/Categories/Categories.jsx
@@ -1,8 +1,9 @@
+import { forwardRef } from 'react';
 import styles from './styles.module.css';
 
-const Categories = ({categories, setSelectedCategory, selectedCategory}) => {
+const Categories = forwardRef(({categories, setSelectedCategory, selectedCategory}, ref) => {
     return (
-        <div className={styles.categories}>
+        <div ref={ref} className={styles.categories}>
             <button
                 onClick={() => setSelectedCategory(null)}
                 className={!selectedCategory
@@ -24,6 +25,8 @@ const Categories = ({categories, setSelectedCategory, selectedCategory}) => {
             })}
         </div>
     );
-};
+});
+
+Categories.displayName = 'Categories'
 
 export default Categories;
